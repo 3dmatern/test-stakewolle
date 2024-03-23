a07b61b96a022fcff14395928ae511dcb4cc0839
Denis Matern <denismatern@gmail.com>
2024-03-23 15:46:37 +0300

    install mui

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,30 @@
+import { Box, Container, Link, Typography } from "@mui/material";
+import NextLink from "next/link";
+
 export default function Home() {
-  return <main>Home</main>;
+  return (
+    <Container
+      maxWidth="lg"
+      sx={{
+        minWidth: "360px",
+      }}
+    >
+      <Box
+        sx={{
+          my: 4,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
+          Material UI - Next.js App Router example in TypeScript
+        </Typography>
+        <Link href="/" color="secondary" component={NextLink}>
+          Go to the home page
+        </Link>
+      </Box>
+    </Container>
+  );
 }
